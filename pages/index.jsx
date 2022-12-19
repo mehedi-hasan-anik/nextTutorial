e8d089ec29185943c89da4eea2668ec7f04b27ea
mehedi-hasan-anik <anikm2283@gmail.com>
2022-12-19 21:45:45 +0600

    done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,24 @@
 import { Inter } from "@next/font/google";
 import Link from "next/Link";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("router", router);
+    router.push("/product");
+  };
   return (
     <div className={styles.main}>
       <h1>Home</h1>
       <Link href="/blog">blog</Link>
       <Link href="/product">product</Link>
+
+      <button onClick={handleClick}>place Order</button>
     </div>
   );
 }
